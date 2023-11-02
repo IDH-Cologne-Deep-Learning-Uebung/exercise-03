98a7fae048108c9a2a93676932a997158a3facc0
Marvin Kamphausen <43293625+mkamphausen@users.noreply.github.com>
2023-11-02 14:24:00 +0100

    func2
    added function 2

--- functions.py
+++ functions.py
@@ -9,8 +9,36 @@ def func1(a, b):
     return None
   return type(a)
 
+# check the number of arguments. 
+# If there are less than two arguments, return a string with the number of arguments. 
+# If there are two arguments, pass them to `func1`. 
+# If there are more than two arguments, return the number of arguments as an int value. 
+def func2(*args):
+  if len(args) < 2:
+    return str(len(args))
+  if len(args) == 2:
+    return func1(*args)
+  if len(args) > 2:
+    return len(args)
+    
+  
+
 func1(1,2)
 func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+print(func1(1,2))
+print(func1("Welt", "Hallo"))
+print(func1(None, None))
+print(func1(11, "Freunde"))
+print(func1(5.4, 3.6))
+
+print("----")
+
+print(func2(2))
+print(func2(2, 3))
+print(func2(4, "3"))
+print(func2("Goodbye", "World"))
+print(func2(2, 3, 3, "test"))

0008ddbf760b00ca11485a6595a2826ea9eb19f9
0xf000000 <Leon@margale.de>
2023-11-02 14:14:08 +0100

    exercise done

--- functions.py
+++ functions.py
@@ -14,3 +14,49 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+
+def func2(*arg):
+  argLen= len(arg)
+
+  if argLen< 2:
+    return str(len(arg))
+  
+  if argLen == 2 :
+    return func1(arg[0], arg[1])
+  
+  if argLen > 2:
+    return int(len(arg))
+
+
+  
+  return None
+
+
+
+
+def func3(*args ):
+  a= None
+  b= None
+  
+  for arg in args:
+    if arg == 'a':
+      a= arg
+    if arg == 'b':
+      b = arg
+  
+  if a is not None and b is not None:
+    
+    return func1(a, b)
+
+
+  return func2(args)
+
+
+#Next, write a function `func3` that takes an arbitrary number of named arguments. Check that two of these arguments have the names `a` and `b`. If so, pass them into `func1`. If not, pass all names of the arguments into `func2`.
+
+#If you haven't figured it out by now: These functions don't make sense, they are just exercise material.
+
+
+
+print(func3("a","b"))
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,22 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+#nr 1 
+a = [j * 2  for j in x ]
+print(a)
+
+b = [j for j in x if (j % 2) == 0 ]
+print(b)
+
+
+c = [j for j in y if j]
+print(c)
+
+
+d = [j for j in y if isinstance(j, str)]
+print(d)
+
+
+e = [ [ True for s in range(j)] for j in x if j % 2 != 0 ]
+
+print(e)

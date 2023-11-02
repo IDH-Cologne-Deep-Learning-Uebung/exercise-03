1fa005f589182c81029407351f6b76dcb1283c7e
thelarsjedi <lmocka@smail.uni-koeln.de>
2023-11-02 19:43:24 +0100

    hoffentlich alles richtig gelöst. Func3 aus Step4 hab ich nicht so hinbekommen glaube ich.

--- functions.py
+++ functions.py
@@ -9,8 +9,60 @@ def func1(a, b):
     return None
   return type(a)
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+print(func1(1,2))
+
+print(func1("Welt", "Hallo"))
+print(func1(None, None))
+print(func1(11, "Freunde"))
+print(func1(5.4, 3.6))
+
+print("ab hier Func2-Ausgabe")
+
+def func2(*arguments):
+  numArgs = len (arguments)
+  
+  if numArgs < 2:
+    return str(numArgs)
+  if numArgs == 2:
+   return func1(arguments[0],arguments[1])
+  if numArgs > 2:
+    return int(numArgs)
+
+print(func2("Welt", 3, 6))
+print(func2(1))
+print(func2(1,4,5,6,3))
+print(func2(3,4))
+print(func2("test", "toller test"))
+print("func2 ende")
+
+
+"""""
+def func3(*argumente):
+  print(argumente)
+  
+  #if argumente == 'a' and argumente == 'b':
+  if 'a' in argumente and 'b' in argumente:
+    return func1(argumente['a'],argumente['b'])
+  else:
+    return func2(argumente)
+    
+print("ab hier func3")
+
+print(func3("a","b","c"))
+print(func3("hej", "Jag talar svenska"))
+print(func3("hi",6,5,6))
+
+"""
+def func3(**argumente):
+    print(argumente)
+    
+    if 'a' in argumente and 'b' in argumente:
+        return func1(argumente['a'], argumente['b'])
+    else:
+        return func2(argumente)
+
+# Beispielaufrufe
+print("ab hier func3")
+print(func3(a="a", b="b", c="c"))
+print(func3(foo="hej", bar="Jag talar svenska"))
+print(func3(x="hi", y=6, z=5, w=6))
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,54 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+#- A list called `a` that contains all elements of `x`, multiplied by 2.
+
+a=[zahl*2 for zahl in x]
+print(a)
+
+#- A list called `b` that contains all even elements of `x`.
+b=[zahl for zahl in x if zahl%2==0]
+print(b)
+
+#- A list called `c` that contains all truish elements of `y` (i.e., all elements that evaluate to `True` in a boolean context)
+c=[value for value in y if value==True]
+print(c)
+
+
+#- A list called `d` that contains all string elements of `y`
+d=[value for value in y if type(value)==str]
+print(d)
+
+#- A list called `e` that contains a list for each odd element of `x`. The number of list elements is given by the current number of `x`, and all inner list elements are `True`. Use I.e., the list `e` starts like this: `[[True], [True, True, True], [True, True, True, True, True], ...]`
+
+"""
+erster Ansatz hat nicht funktioniert
+    e=[]
+    e_odd=[zahl for zahl in x if zahl%2!=0]
+    print("e_odd: ")
+    print(e_odd)
+
+    e=[value for value in e_odd]
+
+    
+
+zweiter Ansatz
+    e=[]
+    #in e kommt eine liste, die für jedes ungerade Element von X eine liste enthält. Die Anzahl der Listenelemte wird durch die aktuelle Nummer von X vorgegeben. Die Inneren Listelemente sind True
+    #1= True
+    #3 = True, True, True
+
+    e=[value for value in x ]
+"""
+
+"""
+e = [[True] * (2 * i + 1) for i in x]
+print(e)
+#pass klappt nicht, nach der 9 wird einfach weiter gemacht und es werden 11 Trues ausgegeben
+"""
+#e = [for element in x if element%2!=1 ] 
+
+#e = [[s for s in range(element for element in x if element % 2 == 0)]]
+e = [[True]*element for element in x if element %2 ==1]
+
+print(e)

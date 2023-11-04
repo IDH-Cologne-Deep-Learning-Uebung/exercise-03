83d54ada94c3c8209ec3ad4e45d8b984664e45b4
lwechsl1 <l-wechsler@protonmail.com>
2023-11-04 18:05:54 +0100

    commit

--- functions.py
+++ functions.py
@@ -14,3 +14,46 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+def func2(*args):
+  args_size = len(args)
+
+  if args_size < 2:
+    print('case1')
+    return str(args_size)
+
+  elif args_size == 2:
+    print('case2')
+    func1(args[0],args[1])
+
+  elif args_size > 2 :
+    print('case3')
+    return args_size
+
+func2(2)
+func2(2,3)
+func2(2,4,4)
+func2(2,23,3,4,4,4,4)
+
+def func3(*args, **kwargs):
+  args_size = len(args)
+
+  if args_size < 2:
+    keylist = []
+    valuelist = []
+
+    for key, value in kwargs.items():
+      keylist += ({key})
+      valuelist += ({value})
+    print(keylist,valuelist)
+
+    if keylist.count("a") == 1 and keylist.count("b") == 1:
+      func1(valuelist[0], valuelist[1])
+      print('a and b')
+
+    else:
+      print('else')
+      func2(valuelist[0], valuelist[1])
+
+func3(a=3,b=4)
+func3(a=3, c=3)
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,29 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = []
+b = []
+c = []
+d = []
+e = []
+
+for element in y:
+    if type(element) == bool:
+        if element:
+            c.append(element)
+    if type(element) == str:
+        d.append(element)
+
+for element in x:
+    a.append(element * 2)
+    if element % 2 == 0:
+        b.append(element)
+    if element % 2 != 0:
+        temp = []
+        int = 0
+        while element > int :
+            temp.append(True)
+            int +=1
+        e.append(temp)
+
+print(a,b,c,d,e)

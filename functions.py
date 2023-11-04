02fd0183b0831ca303ed3f32906961c79f758956
hannahmueller <hmuell52@smail.uni-koeln.de>
2023-11-04 14:57:54 +0100

    Fertig

--- functions.py
+++ functions.py
@@ -9,8 +9,29 @@ def func1(a, b):
     return None
   return type(a)
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+print(func1(1,2))
+print(func1("Welt", "Hallo"))
+print(func1(None, None))
+print(func1(11, "Freunde"))
+print(func1(5.4, 3.6))
+
+def func2(*args):
+  numArgs = len(args)
+  if numArgs < 2:
+    return str(args[0]) + ' ' + str(args[1])
+  if numArgs == 2:
+    func1(*args)
+  if numArgs > 2:
+    return numArgs
+  
+def func3(*args):
+  test = ""
+  for arg in args:
+    if arg == "a":
+      test + "a"
+    if arg == "b":
+      test + "b"
+  if test == "ab" or test == "ba":
+    func1("a", "b")
+  else:
+    func2(args)
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,29 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [z*2 for z in x] # oder statt z x?
+print(a)
+b = [z for z in x if z%2 == 0]
+print(b)
+c = [z for z in y if z == True]
+print(c)
+d = [z for z in y if isinstance(z, str)]
+print(d)
+
+def convert(zahl):
+    liste = []
+    i = zahl
+    while i > 0:
+        liste.append(True)
+        i = i - 1
+    return liste
+
+e = []
+i = 0
+for z in x:
+    if z%2 == 1:
+        e.append([])
+        e[i].append(convert(z))
+        i = i + 1
+print(e)
+

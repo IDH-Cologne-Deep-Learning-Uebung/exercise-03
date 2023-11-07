360346052b8b848e71effae71c7db85586dd55d0
NilsKEA <nallstae@smail.uni-koeln.de>
2023-11-07 13:05:42 +0100

    first

--- functions.py
+++ functions.py
@@ -14,3 +14,19 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+
+def func2(*args):
+  num_args = len(args)
+  if num_args < 2:
+    return f"Numberof Arguments: {num_args}"
+  elif num_args == 2:
+    return func1(*args)
+  else:
+    return int(num_args)
+
+def func3(**kwargs):
+    if 'a' in kwargs and 'b' in kwargs:
+        return func1(kwargs['a'], kwargs['b'])
+    else:
+        return func2(**kwargs)
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,30 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in x]
+print(a)
+
+b=[]
+for zahl in x:
+    if zahl%2 ==0:
+        b.append(zahl)
+print(b)
+
+
+c=[]
+for element in y:
+    if element is True:
+        c.append(element)
+print(c)
+
+
+d=[]
+for item in y:
+    if isinstance(item, str):
+        d.append(item)
+print(d)
+
+e=[]
+e = [[True] * i for i in x if i % 2 == 1]
+
+print(e)

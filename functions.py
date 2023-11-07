0adfaaad511a3f68879718bc878288b963727746
Marius Klossek <marius.klossek@gmail.com>
2023-11-07 22:36:17 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,38 @@
 def func1(a, b):
   if type(a) == int and type(b) == int:
+    print (a+b)
     return a+b
   if type(a) == str and type(b) == str:
+    print(b+" "+a)
     return b+" "+a
   if a is None and b is None:
+    print("Does not exist.")
     return "Does not exist."
   if type(a) != type(b):
+    print(None)
     return None
   return type(a)
+
+def func2(*args):
+  numArgs = len(args)
+  if numArgs < 2:
+    print(str(numArgs) + " arguments were given.")
+    return
+  if numArgs > 2:
+    print(int(numArgs))
+    return 
+  if numArgs == 2:
+    func1(*args)
+
+def func3(**kwargs):
+  if 'a' in kwargs and 'b' in kwargs:
+    func1(kwargs['a'], kwargs['b'])
+  else:
+    func2(*kwargs)
+
+  return
+
+func3(e=1, z=7, c=7, y=4)
 
 func1(1,2)
 func1("Welt", "Hallo")

dc9453b0fe6c4f0ac3a06aa45544ad9546979fea
Mario Müller <mmuel120@smail.uni-koeln.de>
2023-11-14 23:21:50 +0100

    ex3

--- functions.py
+++ functions.py
@@ -14,3 +14,34 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+def func2(*args):
+  numArgs = len(args)
+  if numArgs < 2:
+    print(str(numArgs))
+    return str(numArgs)
+  elif numArgs == 2:
+    func1(args[0],args[1])
+  else:
+    print(int(numArgs))
+    return int(numArgs)
+func2(2)
+func2("Welt", "Hallo")
+func2(None, None)
+func2(11, "Freunde")
+func2(5.4, 3.6, 3)
+
+
+def func3(*kwargs):
+  keyisA = False
+  keyIsB = False
+  for key in kwargs:
+    if key == a:
+      keyIsA = True
+    if key == b:
+      keyIsB = True
+  if keyIsA and keyIsB:
+    func1(kwargs[a],kwargs[b])
+  else:
+    func2(kwargs)
+func3(a=1, b="Hallo", c=0, d=True)
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,37 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = []
+for zahl in x:
+    a.append(zahl * 2)
+
+print(a)
+
+b = []
+for zahl in x:
+    if zahl % 2 == 0:
+        b.append(zahl)
+
+print(b)
+
+c = []
+for element in y:
+    if element:
+        c.append(element)
+print(c)
+
+d = []
+for element in y:
+    if type(element) == str:
+        d.append(element)
+print(d)
+
+e = []
+for zahl in x:
+    if zahl % 2 == 0:
+        list = []
+        for x in range(zahl):
+            list.append(True)
+        e.append(list)
+print(e)
+
